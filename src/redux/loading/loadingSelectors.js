23623a699a35ddfcd1e66329403aa9c7185fe709
RomanPidlisnychyi <myLuckyhelper@gmail.com>
2021-02-19 16:53:50 +0200

    loading selectors fix

--- src/redux/loading/loadingSelectors.js
+++ src/redux/loading/loadingSelectors.js
@@ -1,9 +1,1 @@
-const getUserName = state => state.auth.user.name;
-const getAuthErr = state => state.auth.error;
-const getRecoveryStatus = state => state.auth.status;
-
-export default {
-  getUserName,
-  getAuthErr,
-  getRecoveryStatus,
-};
+export default state => state.loading;
