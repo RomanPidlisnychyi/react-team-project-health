41ed59afed04e742c4f17e02a53ffa792c633f3e
Lisnyk-M <swapbuffers@gmail.com>
2021-03-06 21:04:49 +0200

    deleted from tmp branches rations and rationsitem components

--- src/components/Dairy/ProductsInput/ProductsInput.js
+++ src/components/Dairy/ProductsInput/ProductsInput.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styles from './ProductsInput.module.css';
-import rationsItemOperations from '../../../redux/dairy/rationsItemOperations';
-// const debounce = require('lodash.debounce');
 
 const ProductsInput = ({ productSearchValue, onChangeInput, onInputClick , disabled}) => {
     return (
@@ -14,31 +12,14 @@ const ProductsInput = ({ productSearchValue, onChangeInput, onInputClick , disab
             value={productSearchValue}
             onChange={onChangeInput}
             disabled={disabled}
-            // onClick={onInputClick}
         />
     )
 }
 
 const mapStateToProps = (state, ownProps) => ({
-    // visibleListProducts: state.visibleListProducts,
-    // productSearchValue: state.productSearchValue,
 })
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-    // onInputClick: () => dispatch(rationsItemOperations.unsetVisibleList()),
-    // onChangeInput: (e) => {
-    //     // debounce(dispatch, 300)
-    //     // const debouncedDispatch = debounce(dispatch, 3000, {
-    //     //     leading: false,
-    //     //     trailing: true
-    //     // });
-    //     // const timer = setTimeout(dispatch(rationsItemOperations.getProducts(e)), 300);
-    //     // dispatch(clearTimeout(timer));
-
-    //     // dispatch(rationsItemOperations.getProducts(e.target.value));
-    //     // dispatch(rationsItemOperations.productSearchValueChange(e.target.value));    
-    //     // dispatch(rationsItemOperations.setVisibleList());
-    // },    
+const mapDispatchToProps = (dispatch, ownProps) => ({    
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProductsInput);
