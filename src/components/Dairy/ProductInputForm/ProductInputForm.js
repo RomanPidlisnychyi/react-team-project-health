31c27c5297f083c8eaa9478548c630e8fde81c98
RomanPidlisnychyi <myluckyhelper@gmail.com>
2021-03-08 17:26:40 +0200

    date complete

--- src/components/Dairy/ProductInputForm/ProductInputForm.js
+++ src/components/Dairy/ProductInputForm/ProductInputForm.js
@@ -12,245 +12,268 @@ import ReactNotification from 'react-notifications-component';
 import 'react-notifications-component/dist/theme.css';
 import sendNotification from './notification';
 import rationsItemOperationsVit from '../../../redux/rations/rationItemsOperations';
+import { rationItemsSelectors } from '../../../redux/rations';
 // import debounce from 'lodash.debounce';
 
 class ProductInputForm extends Component {
-    constructor(props) {
-        super(props);
-        const nowDate = new Date();
-        const year = nowDate.getFullYear().toString();
-        const month = (nowDate.getMonth() + 1).toString();
-        const day = nowDate.getDate().toString();
+  constructor(props) {
+    super(props);
+    const nowDate = new Date();
+    const year = nowDate.getFullYear().toString();
+    const month = (nowDate.getMonth() + 1).toString();
+    const day = nowDate.getDate().toString();
 
-        const trDay = day.length === 2 ? day : '0' + day;
-        const trMonth = month.length === 2 ? month : '0' + month;
-        this.state.date = year + '-' + trMonth + '-' + trDay;
+    const trDay = day.length === 2 ? day : '0' + day;
+    const trMonth = month.length === 2 ? month : '0' + month;
+
+    const { userDate } = this.props;
+
+    const newDate = userDate
+      ? userDate.split('-').reverse().join('-')
+      : year + '-' + trMonth + '-' + trDay;
+
+    this.state.date = newDate;
+  }
+
+  state = {
+    date: '',
+    weight: '',
+    productTitle: '',
+    productSearchValue: '',
+    visibleListProducts: false,
+    visibleNotification: false,
+    notificationMessage: '',
+    products: [],
+    productInFocusId: 0,
+    buttonAddDisabled: false,
+    productInputDisabled: false,
+    WeightInputDisabled: false,
+    buttonText: 'Добавить',
+  };
+
+  componentDidMount() {
+    document.addEventListener('click', this.addBodyClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.addBodyClick);
+  }
+
+  addBodyClick = e => {
+    //if click under productList then ignore click
+    if (!e.target.parentElement.id === 'productList') {
+      return;
     }
 
-    state = {
-        date: '',
-        weight: '',
-        productTitle: '',
+    this.setState({ visibleListProducts: false });
+  };
+
+  handleItemHover = e => {
+    const keys = [];
+    for (const [key, value] of Object.entries(e.target)) {
+      keys.push(key);
+    }
+    this.setState({ productInFocusId: e.target[keys[1]].value });
+  };
+
+  handleChangeWeight = e => {
+    this.setState({ weight: e.target.value });
+  };
+
+  handleChangeProducts = async e => {
+    this.setState({
+      productSearchValue: e.target.value,
+      visibleListProducts: true,
+    });
+
+    try {
+      const products = await rationsItemOperations.getProducts(e.target.value);
+      this.setState({ products });
+    } catch (error) {
+      sendNotification(error.message);
+      this.setState({ visibleListProducts: false });
+    }
+  };
+
+  handlerChangeDate = async e => {
+    const date = e.target.value.split('-');
+    const transformedDate = [date[2], date[1], date[0]].join('-');
+
+    this.setState({
+      date: e.target.value,
+      transformedDate,
+    });
+
+    // try {
+    await this.props.onGetInfo(transformedDate);
+    // } catch (error) {
+    // sendNotification(error.payload.message || 'По указанной дате нет записей');
+    // }
+  };
+
+  handleProductInputClick = () => {
+    this.setState({
+      productSearchValue: this.state.productInFocusId,
+      visibleListProducts: false,
+    });
+  };
+
+  // handleAddProductClick
+  handleSubmit = async e => {
+    e.preventDefault();
+    const {
+      transformedDate,
+      weight,
+      productSearchValue,
+      buttonAddDisabled,
+    } = this.state;
+    const { onRationsItemAdd, onGetInfo, onRationsItemUpdate } = this.props;
+
+    const ration = {
+      date: transformedDate,
+      productTitle: productSearchValue,
+      weight: Number(weight),
+    };
+    // const fail = objectRequestValidator(ration);
+
+    // if (fail) {
+    //     const failMessage = fail.details[0].message;
+
+    //     this.setState({
+    //         visibleNotification: true,
+    //         notificationMessage: failMessage,
+    //     });
+
+    //     sendNotification(failMessage, 'danger');
+    //     return;
+    // }
+
+    try {
+      this.setState({
+        buttonAddDisabled: true,
+        productInputDisabled: true,
+        WeightInputDisabled: true,
+        buttonText: 'Добавление',
+      });
+
+      const rr = await onRationsItemAdd(ration);
+      onRationsItemUpdate(rr.payload);
+      sendNotification(`Продукт ${rr.payload.title} удачно добавлен` || 'ок!');
+
+      this.setState({
         productSearchValue: '',
-        visibleListProducts: false,
-        visibleNotification: false,
-        notificationMessage: '',
-        products: [],
-        productInFocusId: 0,
+        weight: '',
+      });
+    } catch (error) {
+      sendNotification(error.payload.message || 'error');
+    } finally {
+      this.setState({
         buttonAddDisabled: false,
         productInputDisabled: false,
         WeightInputDisabled: false,
         buttonText: 'Добавить',
-    };
-
-    componentDidMount() {
-        document.addEventListener('click', this.addBodyClick);
+      });
     }
+  };
 
-    componentWillUnmount() {
-        document.removeEventListener('click', this.addBodyClick);
-    }
+  render() {
+    const {
+      weight,
+      date,
+      visibleListProducts,
+      productSearchValue,
+      products,
+      buttonAddDisabled,
+      visibleNotification,
+      productInputDisabled,
+      WeightInputDisabled,
+      buttonText,
+    } = this.state;
 
-    addBodyClick = e => {
-        //if click under productList then ignore click
-        if (!e.target.parentElement.id === 'productList') {
-            return;
-        }
+    const { mode } = this.props;
 
-        this.setState({ visibleListProducts: false });
-    };
+    // const classDatePickerWrapper = `${styles.datePickerWrapper}
+    //  ${mode === 'usual' ? styles.enabled : styles.disabled}`
 
-    handleItemHover = e => {
-        const keys = [];
-        for (const [key, value] of Object.entries(e.target)) {
-            keys.push(key);
-        }
-        this.setState({ productInFocusId: e.target[keys[1]].value });
-    };
+    const classUsualEnabled = `${
+      mode === 'usual' ? styles.visible : styles.hidden
+    }`;
+    const classUsualDisabled = `${
+      mode === 'modal' ? styles.visible : styles.hidden
+    }`;
+    const classButton = `${
+      mode === 'usual' ? styles.buttonUsual : styles.buttonModal
+    }`;
+    const classInputPanelWrapper = `${
+      mode === 'usual'
+        ? styles.inputPanelWrapperUsual
+        : styles.inputPanelWrapperModal
+    }`;
 
-    handleChangeWeight = e => {
-        this.setState({ weight: e.target.value });
-    };
+    console.log('classes: ', classButton);
 
-    handleChangeProducts = async e => {
-        this.setState({
-            productSearchValue: e.target.value,
-            visibleListProducts: true,
-        });
+    return (
+      <form
+        id="form-products"
+        className={styles.form}
+        onSubmit={this.handleSubmit}
+      >
+        <div className={(styles.datePickerWrapper, classUsualEnabled)}>
+          <Picker date={date} onChangeData={this.handlerChangeDate} />
+        </div>
 
-        try {
-            const products = await rationsItemOperations.getProducts(e.target.value);
-            this.setState({ products });
-        } catch (error) {
-            sendNotification(error.message);
-            this.setState({ visibleListProducts: false });
-        }
-    };
+        <div className={classInputPanelWrapper}>
+          <ProductsInput
+            productSearchValue={productSearchValue}
+            onChangeInput={this.handleChangeProducts}
+            disabled={productInputDisabled}
+          />
 
-    handlerChangeDate = async e => {
-        const date = e.target.value.split('-');
-        const transformedDate = [date[2], date[1], date[0]].join('-');
+          <WeightInput
+            weight={weight}
+            onChangeWeight={this.handleChangeWeight}
+            disabled={WeightInputDisabled}
+          />
+          {/* {className={styles.button, classUsualEnabled}} */}
+          <div className={classButton}>
+            <Button
+              type="submit"
+              title={buttonText}
+              onClick={this.handleAddProductClick}
+              disabled={buttonAddDisabled}
+            />
+          </div>
 
-        this.setState({
-            date: e.target.value,
-            transformedDate,
-        });
+          {products && products.length > 0 && visibleListProducts && (
+            <ProductsList
+              products={products}
+              onHover={this.handleItemHover}
+              onInputClick={this.handleProductInputClick}
+              onMouseOut={this.handleProductsListMouseOut}
+            />
+          )}
 
-        // try {
-        await this.props.onGetInfo(transformedDate);
-        // } catch (error) {
-        // sendNotification(error.payload.message || 'По указанной дате нет записей');
-        // }
-    };
-
-    handleProductInputClick = () => {
-        this.setState({
-            productSearchValue: this.state.productInFocusId,
-            visibleListProducts: false,
-        });
-    };
-
-    // handleAddProductClick
-    handleSubmit = async e => {
-        e.preventDefault();
-        const {
-            transformedDate,
-            weight,
-            productSearchValue,
-            buttonAddDisabled,
-        } = this.state;
-        const { onRationsItemAdd, onGetInfo, onRationsItemUpdate } = this.props;
-
-        const ration = {
-            date: transformedDate,
-            productTitle: productSearchValue,
-            weight: Number(weight),
-        };
-        // const fail = objectRequestValidator(ration);
-
-        // if (fail) {
-        //     const failMessage = fail.details[0].message;
-
-        //     this.setState({
-        //         visibleNotification: true,
-        //         notificationMessage: failMessage,
-        //     });
-
-        //     sendNotification(failMessage, 'danger');
-        //     return;
-        // }
-
-        try {
-            this.setState({
-                buttonAddDisabled: true,
-                productInputDisabled: true,
-                WeightInputDisabled: true,
-                buttonText: 'Добавление',
-            });
-
-            const rr = await onRationsItemAdd(ration);
-            onRationsItemUpdate(rr.payload);
-            sendNotification(`Продукт ${rr.payload.title} удачно добавлен` || 'ок!');
-
-            this.setState({
-                productSearchValue: '',
-                weight: '',
-            });
-
-        } catch (error) {
-            sendNotification(error.payload.message || 'error');
-
-        } finally {
-            this.setState({
-                buttonAddDisabled: false,
-                productInputDisabled: false,
-                WeightInputDisabled: false,
-                buttonText: 'Добавить',
-            })
-        }
-    }
-
-    render() {
-        const {
-            weight, date, visibleListProducts, productSearchValue,
-            products, buttonAddDisabled, visibleNotification,
-            productInputDisabled, WeightInputDisabled, buttonText,
-        } = this.state;
-
-        const { mode } = this.props;
-
-        // const classDatePickerWrapper = `${styles.datePickerWrapper}
-        //  ${mode === 'usual' ? styles.enabled : styles.disabled}`
-
-        const classUsualEnabled = `${mode === 'usual' ? styles.visible : styles.hidden}`;
-        const classUsualDisabled = `${mode === 'modal' ? styles.visible : styles.hidden}`;
-        const classButton = `${mode === 'usual' ? styles.buttonUsual : styles.buttonModal}`;
-        const classInputPanelWrapper = `${mode === 'usual'
-            ? styles.inputPanelWrapperUsual : styles.inputPanelWrapperModal}`;
-
-        console.log('classes: ', classButton)
-
-        return <form id="form-products" className={styles.form} onSubmit={this.handleSubmit}>
-            <div className={styles.datePickerWrapper, classUsualEnabled}>
-                <Picker date={date} onChangeData={this.handlerChangeDate} />
-            </div>
-
-            <div className={classInputPanelWrapper}>
-                <ProductsInput
-                    productSearchValue={productSearchValue}
-                    onChangeInput={this.handleChangeProducts}
-                    disabled={productInputDisabled}
-                />
-
-                <WeightInput
-                    weight={weight}
-                    onChangeWeight={this.handleChangeWeight}
-                    disabled={WeightInputDisabled}
-                />
-                {/* {className={styles.button, classUsualEnabled}} */}
-                <div className={classButton}>
-                    <Button
-                        type="submit"
-                        title={buttonText}
-                        onClick={this.handleAddProductClick}
-                        disabled={buttonAddDisabled}
-                    />
-                </div>
-
-                {products && products.length > 0 && visibleListProducts &&
-                    <ProductsList
-                        products={products}
-                        onHover={this.handleItemHover}
-                        onInputClick={this.handleProductInputClick}
-                        onMouseOut={this.handleProductsListMouseOut}
-                    />
-                }
-
-                {visibleNotification && <ReactNotification />}
-            </div>
-        </form >
-    }
-};
-
-
-
+          {visibleNotification && <ReactNotification />}
+        </div>
+      </form>
+    );
+  }
+}
 
 // const mapStateToProps = (state, ownProps) => ({});
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-    onRationsItemAdd: param =>
-        dispatch(rationsItemOperations.rationsItemAdd(param)),
-    // onGetInfo: (param) => dispatch(rationsItemOperations.getInfoByDate(param)),
-    onGetInfo: param =>
-        dispatch(rationsItemOperationsVit.fetchRationItems(param)),
-    // onGetInfo: (param) => dispatch(rationsItemOperations.getInfoByDate(param)),
-    onRationsItemUpdate: param =>
-        dispatch(rationsItemOperationsVit.rationsItemUpdate(param)),
+  onRationsItemAdd: param =>
+    dispatch(rationsItemOperations.rationsItemAdd(param)),
+  // onGetInfo: (param) => dispatch(rationsItemOperations.getInfoByDate(param)),
+  onGetInfo: param =>
+    dispatch(rationsItemOperationsVit.fetchRationItems(param)),
+  // onGetInfo: (param) => dispatch(rationsItemOperations.getInfoByDate(param)),
+  onRationsItemUpdate: param =>
+    dispatch(rationsItemOperationsVit.rationsItemUpdate(param)),
 });
 const mapStateToProps = (state, ownProps) => ({
-    userName: state.auth.user.name
+  userDate: rationItemsSelectors.getRationDate(state),
 });
-
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProductInputForm);
--- src/redux/dairy/rationsItemOperations.js
+++ src/redux/dairy/rationsItemOperations.js
@@ -1,57 +1,73 @@
 import Axios from 'axios';
 import rationsItemActions from './rationsItemActions';
 import HEROKU from '../../services/apiURL';
+import generateCurrentDate from '../../services/generateCurrentDate';
 
 const getInfoByDate = date => dispatch => {
-    dispatch(rationsItemActions.getInfoByDateRequest());
+  dispatch(rationsItemActions.getInfoByDateRequest());
 
-    return new Promise ((res, rej) => {
-        Axios(`${HEROKU}/users/infobyday/${date}`)
-        .then(response => res(dispatch(rationsItemActions.getInfoByDateSuccess(response.data))))
-        .catch(error => rej(dispatch(rationsItemActions.getInfoByDateError(error.response.data))));
-    })    
-}
+  return new Promise((res, rej) => {
+    Axios(`${HEROKU}/users/infobyday/${date}`)
+      .then(response =>
+        res(dispatch(rationsItemActions.getInfoByDateSuccess(response.data))),
+      )
+      .catch(error =>
+        rej(
+          dispatch(rationsItemActions.getInfoByDateError(error.response.data)),
+        ),
+      );
+  });
+};
 
 const rationsItemAdd = param => dispatch => {
-    const options = {
-        date: param.date,
-        productTitle: param.productTitle,
-        weight: param.weight,
-    }
-    dispatch(rationsItemActions.rationsItemAddRequest(param));
+  const date = param.date ? param.date : generateCurrentDate();
 
-    return new Promise ((res, rej) => {
-        Axios.post(`${HEROKU}/rations`, options)
-        .then(response => res(dispatch(rationsItemActions.rationsItemAddSuccess(response.data))))
-        .catch(error => rej(dispatch(rationsItemActions.rationsItemAddError(error.response.data))))
-    })
-}
+  const options = {
+    date,
+    productTitle: param.productTitle,
+    weight: param.weight,
+  };
+
+  dispatch(rationsItemActions.rationsItemAddRequest(param));
+
+  return new Promise((res, rej) => {
+    Axios.post(`${HEROKU}/rations`, options)
+      .then(response =>
+        res(dispatch(rationsItemActions.rationsItemAddSuccess(response.data))),
+      )
+      .catch(error =>
+        rej(
+          dispatch(rationsItemActions.rationsItemAddError(error.response.data)),
+        ),
+      );
+  });
+};
 
 const rationsItemUpdate = param => dispatch => {
-    dispatch(rationsItemActions.rationsItemUpdate(param));
-}
+  dispatch(rationsItemActions.rationsItemUpdate(param));
+};
 
 const dateToStore = param => dispatch => {
-    console.log('dateToStore: ', param)
-    dispatch(rationsItemActions.dateToStore(param));
-}
+  console.log('dateToStore: ', param);
+  dispatch(rationsItemActions.dateToStore(param));
+};
 
 const getProducts = spr => {
-    if (spr === '' || spr.length === 1) {
-        return;
-    }
+  if (spr === '' || spr.length === 1) {
+    return;
+  }
 
-    return new Promise((res, rej) => {
-        Axios.get(`${HEROKU}/products?${spr}`)
-            .then(response => res(response.data))
-            .catch(error => rej(error.response.data))
-    })
-}
+  return new Promise((res, rej) => {
+    Axios.get(`${HEROKU}/products?${spr}`)
+      .then(response => res(response.data))
+      .catch(error => rej(error.response.data));
+  });
+};
 
 export default {
-    getInfoByDate,
-    getProducts,
-    rationsItemAdd,
-    rationsItemUpdate,
-    dateToStore,
-}
+  getInfoByDate,
+  getProducts,
+  rationsItemAdd,
+  rationsItemUpdate,
+  dateToStore,
+};
