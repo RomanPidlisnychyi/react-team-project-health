e6b1df0a3272bb8d41878e791761a5da647c0ad8
Serhii777 <muzykasv72@gmail.com>
2021-03-09 16:02:21 +0200

    Fixed css

--- src/components/CalculatorCalories/CalculatorCalories.js
+++ src/components/CalculatorCalories/CalculatorCalories.js
@@ -1,33 +1,17 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import styles from './CalculatorCalories.module.css';
 import CaloriesForm from '../CaloriesForm/CaloriesForm';
 
-import { authOperations, authSelectors } from '../../redux/auth';
-
-
-const СalculatorCalories = ({userParams}) => {
-console.log("userParams:", userParams);
-
-
-
+const СalculatorCalories = () => {
   return (
     <div className={styles.wrapperСalculator}>
       <h1 className={styles.titleCalculator}>
         Узнай свою суточную норму калорий
       </h1>
-      <CaloriesForm  />
-
-
+      <CaloriesForm />
     </div>
   );
 };
 
-// const mapStateToProps = state => ({
-//   userParams: authSelectors.getParams(state),
-// });
-
-
 export default СalculatorCalories;
-// export default connect(mapStateToProps, null)(СalculatorCalories);
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,128 +1,21 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
-import PropTypes from 'prop-types';
-
+import React from 'react';
 import styles from './Home.module.css';
 
-import Button from '../Button/Button';
 import CaloriesForm from '../CaloriesForm/CaloriesForm';
 
-import {
-  notrecomendedproductsOperations,
-  notrecomendedproductsSelectors,
-} from '../../redux/notrecomendedproducts';
-import { authOperations } from '../../redux/auth';
-import { modalActions, modalSelectors } from '../../redux/modal/';
-
-import Modal from '../Modal/Modal';
-import ModalCalories from '../Modal/ModalCalories';
-
-class Home extends Component {
-  // static propTypes = {
-  //   height: PropTypes.number,
-  //   age: PropTypes.number,
-  //   currentWeight: PropTypes.number,
-  //   desiredWeight: PropTypes.number,
-  //   bloodGroup: PropTypes.number,
-  // };
-
-  state = {
-    //   height: '',
-    //   age: '',
-    //   currentWeight: '',
-    //   desiredWeight: '',
-    //   bloodGroup: '',
-    showModal: false,
-  };
-
-  // //! notRecomendedProducts
-  // onGetListNotRecomendedProductsAndCalories(userParams).then(data => {
-  //   if (data) {
-  //     console.log(
-  //       `NotRecomendedProducts of user saved successfully: param: ${data}`,
-  //     );
-  //     return;
-  //   }
-  //   console.log('Щось пішло не так! Спробуйте ввести параметри ще раз!');
-  //   // this.handleClearForm(e);
-  // });
-
-  //! userParams
-  // onAddUserParams(userParams).then(data => {
-  //   if (data) {
-  //     console.log(`Parametrs of user saved successfully: param: ${data}`);
-  //     return;
-  //   }
-  //   console.log('Щось пішло не так! Спробуйте ввести параметри ще раз!');
-  //              this.handleClearForm(e);
-  // });
-
-  // console.log('thisSubmit.props:', this.props);
-  // console.log('thisSubmit.state:', this.state);
-
-  // this.showModalCalories();
-  // };
-
-  componentDidUpdate() {
-    this.showModalCalories = () => {
-      // this.props.isModal({ isModal: true });
-      // dispatch(modalActions.onModal())
-      // this.props.showModal({ showModal: true });
-      // this.setState({ showModal: this.state.showModal });
-      // this.setState({ showModal: true });
-      // console.log('thisShow.state:', this.state);
-      // onClick={() => dispatch(modalActions.onModal())}
-    };
-
-    this.handleModal = () => {
-      // console.log('thisShow.props:', this.props);
-      this.setState({ showModal: !this.state.showModal });
-    };
-  }
-
-  render() {
-    // console.log('thisRender.props:', this.props);
-    // console.log('thisRender.state:', this.state);
-
-    const { dailyCalorieNormInteger, listNotProducts, isModal } = this.props;
-
-    return (
-      <>
-        <div className={styles.wrapper}>
-          <h2 className={styles.titleForm}>
-            Просчитай свою суточную норму калорий прямо сейчас
-          </h2>
-          <div className={styles.wrapperForm}>
-            <CaloriesForm />
-          </div>
+const Home = () => {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <h2 className={styles.titleForm}>
+          Просчитай свою суточную норму калорий прямо сейчас
+        </h2>
+        <div className={styles.wrapperForm}>
+          <CaloriesForm />
         </div>
-        {/* {this.state.showModal && (
-          <Modal handleModal={this.handleModal}>
-            <ModalCalories
-              calories={dailyCalorieNormInteger}
-              listNotRecomendedProducts={listNotProducts}
-            />
-          </Modal>
-        )} */}
-      </>
-    );
-  }
-}
-
-const mapStateToProps = state => ({
-  dailyCalorieNormInteger: notrecomendedproductsSelectors.getDailyCalorieNormInteger(
-    state,
-  ),
-  listNotProducts: notrecomendedproductsSelectors.getListNotProducts(state),
-});
-
-const mapDispatchToProps = {
-  onGetListNotRecomendedProductsAndCalories:
-    notrecomendedproductsOperations.getListNotRecomendedProductsAndCalories,
-  onAddUserParams: authOperations.params,
-  // isModal: modalActions.offModal,
-  // isModal: modalActions.onModal,
+      </div>
+    </>
+  );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
