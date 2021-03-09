9d70de1b175452e7c13dcbebabf90067a6ca412c
Vitalii <vzntst@gmail.com>
2021-03-09 23:58:50 +0200

    vit 9

--- src/components/NotRecommended/NotRecommendedProductsList/NotRecommendedProductsList.js
+++ src/components/NotRecommended/NotRecommendedProductsList/NotRecommendedProductsList.js
@@ -1,18 +1,32 @@
 import styles from '../NotRecommended.module.css';
 
-export default function NotRecommendedProductsList({ products, onModalClose }) {
+export default function NotRecommendedProductsList({
+  products,
+  category,
+  onModalClose,
+}) {
   return (
-    <div className={styles.categoryBox}>
-      <ul className={styles.list}>
-        {products.map(product => (
-          <li className={styles.item} key={product}>
-            {product}
-          </li>
-        ))}
-      </ul>
-      <button type="button" onClick={() => onModalClose(false)}>
-        Понятно
-      </button>
+    <div className={styles.modWrapper}>
+      <p className={styles.modTitle}>
+        Нерекомендуемые по категории:{' '}
+        <span className={styles.modSpan}>{category}</span>
+      </p>
+      <div className={styles.modBox}>
+        <ul className={styles.modList}>
+          {products.map(product => (
+            <li className={styles.item} key={product}>
+              {product}
+            </li>
+          ))}
+        </ul>
+        <button
+          type="button"
+          className={styles.modButton}
+          onClick={() => onModalClose(false)}
+        >
+          Понятно
+        </button>
+      </div>
     </div>
   );
 }
