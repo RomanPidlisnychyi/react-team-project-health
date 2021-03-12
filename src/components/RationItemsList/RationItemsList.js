f2b19ad8b79535d0368400ce4e8f0e736f8b38b7
RomanPidlisnychyi <myluckyhelper@gmail.com>
2021-03-12 19:17:25 +0200

    fade

--- src/components/RationItemsList/RationItemsList.js
+++ src/components/RationItemsList/RationItemsList.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import RationItem from '../RationItem/RationItem';
-// import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import styles from './RationItemsList.module.css';
-// import groupStyles from '../Transitions/group.module.css';
 import PropTypes from 'prop-types';
 import rationItemsSelectors from '../../redux/rations/rationItemsSelectors';
 
 function RationItemsList({ rationItems }) {
   return (
-    <>
-      {rationItems && rationItems.length > 0 && (
-        <ul className={styles.list}>
-          {/* <TransitionGroup component="ul" /*className={styles.list}*/}
-          {rationItems.map(item => (
-            // <CSSTransition key={contact.id} timeout={250} /*classNames={groupStyles}*/>
-            <RationItem key={item._id} item={item} />
-            // </CSSTransition>
-          ))}
-          {/* </TransitionGroup> */}
-        </ul>
-      )}
-    </>
+    <TransitionGroup
+      component="ul"
+      className={styles.list}
+      in={rationItems.length}
+    >
+      {rationItems.map(item => (
+        <CSSTransition key={item.title} classNames={styles} timeout={250}>
+          <RationItem item={item} />
+        </CSSTransition>
+      ))}
+    </TransitionGroup>
   );
 }
 
