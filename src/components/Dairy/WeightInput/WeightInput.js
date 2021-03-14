6159c0d48126610522e427ab11e0b81975242df9
RomanPidlisnychyi <myluckyhelper@gmail.com>
2021-03-14 19:21:15 +0200

    styles

--- src/components/Dairy/WeightInput/WeightInput.js
+++ src/components/Dairy/WeightInput/WeightInput.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import styles from './WeightInput.module.css';
 
-const WeightInput = ({onChangeWeight, weight, disabled}) =>
-    <input name="weight"
-        type="number"
-        min="10"
-        max="1000"
-        placeholder="Граммы"
-        className={styles.input}
-        onChange={onChangeWeight}
-        value={weight}
-        disabled={false}
+const WeightInput = ({ onChangeWeight, weight, disabled }) => (
+  <label className={styles.inputLabel}>
+    <input
+      name="weight"
+      type="number"
+      min="10"
+      max="1000"
+      placeholder="Граммы"
+      className={styles.input}
+      onChange={onChangeWeight}
+      value={weight}
+      disabled={false}
     />
+  </label>
+);
 
 export default WeightInput;
