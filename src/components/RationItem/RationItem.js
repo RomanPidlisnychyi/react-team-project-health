cf93b41d229352714564e7319affd7d6b1dd9783
RomanPidlisnychyi <myluckyhelper@gmail.com>
2021-03-14 13:24:13 +0200

    racion item update

--- src/components/RationItem/RationItem.js
+++ src/components/RationItem/RationItem.js
@@ -3,12 +3,22 @@ import { connect } from 'react-redux';
 import rationItemsOperations from '../../redux/rations/rationItemsOperations';
 import styles from './RationItem.module.css';
 import rationItemsSelectors from '../../redux/rations/rationItemsSelectors';
+import { authSelectors } from '../../redux/auth';
 
-const RationItem = ({ item, date, onDeleteItem }) => {
-  const { title, weight, calories } = item;
+const RationItem = ({ item, bloodGroup, date, onDeleteItem }) => {
+  const { title, weight, calories, groupBloodNotAllowed } = item;
+  const isProductNotRecommended = groupBloodNotAllowed[bloodGroup];
   return (
     <li className={styles.item}>
-      <p className={styles.title}>{title}</p>
+      <p
+        className={
+          isProductNotRecommended
+            ? `${styles.title} ${styles.notRecommended}`
+            : styles.title
+        }
+      >
+        {title}
+      </p>
       <p className={styles.weight}>{weight} г</p>
       <p className={styles.calories}>
         {calories} <span className={styles.span}>ккал</span>
@@ -26,6 +36,7 @@ const RationItem = ({ item, date, onDeleteItem }) => {
 
 const mapStateToProps = state => ({
   date: rationItemsSelectors.getRationDate(state),
+  bloodGroup: authSelectors.getParams(state).bloodGroup,
 });
 
 const mapDispatchToProps = {
